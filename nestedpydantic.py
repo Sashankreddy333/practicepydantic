3780794ace0adac0ca9218f381539716c5db46e5
sashank_reddy <sashankreddy238@gmail.com>
2026-01-20 19:08:35 +0530

    serialisation added

--- nestedpydantic.py
+++ nestedpydantic.py
@@ -11,4 +11,9 @@ address_info={"city":"tad","pincode":515411,"state":"andhra"}
 new_address=address(**address_info)
 patient_info={"name":"ram","age":21,"address":new_address}
 new_patient=patient(**patient_info)
-print(new_patient.address.state)
+val1=new_patient.model_dump(exclude=["name"])
+val2=new_patient.model_dump_json(include=["name",address])
+print (val1)
+print(type(val1))
+print(val2)
+print(type(val2))
