f81fc5d6deb3e42d107f67bf5097d89663cd6990
sashank_reddy <sashankreddy238@gmail.com>
2026-01-20 18:54:26 +0530

    computed field feature added

--- modelvalidator.py
+++ modelvalidator.py
@@ -1,16 +1,24 @@
-from pydantic import BaseModel, EmailStr,AnyUrl,Field,field_validator,model_validator
+from pydantic import BaseModel, EmailStr,AnyUrl,Field,field_validator,model_validator,computed_field
 from typing import List,Dict,Optional,Annotated
 
 class patient(BaseModel):
     name:str
     age:Annotated[int,Field(strict=True)]
     email:EmailStr
+    weight:int
+    height:float
     @model_validator(mode="after")
     @classmethod
     def checksomedetails(cls, model):
         if 10<model.age<100 and len(model.name)<20:
             return model
         raise ValueError("check age and name once again")
-patient_info={"name":"ram","age":21,"email":"raj@hdfc.com"}
+    @computed_field
+    @property
+    def bmi(self)->float:
+        bmival=round(self.weight/self.height,2)
+        return bmival
+    
+patient_info={"name":"ram","age":21,"email":"raj@hdfc.com","height":2.1,"weight":30}
 new_patient=patient(**patient_info)
-print(new_patient)  
+print(new_patient.bmi)  
